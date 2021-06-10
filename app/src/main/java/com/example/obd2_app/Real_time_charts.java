58a219cf991ba18c6fbb5276c9f33cdbdc9655e2
EwaKopec <ewa21019@gmail.com>
2021-06-10 17:47:22 +0200

    łooooogień

--- app/src/main/java/com/example/obd2_app/Real_time_charts.java
+++ app/src/main/java/com/example/obd2_app/Real_time_charts.java
@@ -7,12 +7,25 @@ import android.os.Bundle;
 import com.github.anastr.speedviewlib.Speedometer;
 
 public class Real_time_charts extends AppCompatActivity {
-    Speedometer speedometer;
+    Speedometer speedometer, turnover;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_time_charts);
 
       speedometer = findViewById(R.id.awesomeSpeedometer);
+      turnover = findViewById(R.id.turnover);
+    }
+
+    void customizeSpeedometer(Speedometer s)
+    {
+        //s.setBackgroundCircleColor(2);
+        s.setEndDegree(250);
+
+    }
+
+    void customizeTurnover(Speedometer s)
+    {
+
     }
 }
