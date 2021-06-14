0efbb67d2bd8fcddf62a936750a96429a25efe7c
EwaKopec <ewa21019@gmail.com>
2021-06-14 11:49:27 +0200

    customize speedometers, gauge

--- app/src/main/java/com/example/obd2_app/MainActivity.java
+++ app/src/main/java/com/example/obd2_app/MainActivity.java
@@ -2,30 +2,22 @@ package com.example.obd2_app;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import me.aflak.bluetooth.Bluetooth;
 import me.aflak.bluetooth.interfaces.BluetoothCallback;
-import me.aflak.bluetooth.interfaces.DeviceCallback;
 import me.aflak.bluetooth.interfaces.DiscoveryCallback;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, ListView.OnItemClickListener
