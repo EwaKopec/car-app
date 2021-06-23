e7518e4a102be252fef8981cd584de37c81b659c
EwaKopec <ewa21019@gmail.com>
2021-06-23 19:07:10 +0200

    add menu colors

--- app/src/main/java/com/example/obd2_app/Real_time_charts.java
+++ app/src/main/java/com/example/obd2_app/Real_time_charts.java
@@ -3,9 +3,11 @@ package com.example.obd2_app;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.ContextThemeWrapper;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -191,7 +193,8 @@ public class Real_time_charts extends AppCompatActivity implements PopupMenu.OnM
     }
 
     public void onMenuClick(View view) {
-        PopupMenu popupMenu = new PopupMenu(this, view);
+        Context wrapper = new ContextThemeWrapper(this, R.style.ListFont);
+        PopupMenu popupMenu = new PopupMenu(wrapper, view);
         popupMenu.setOnMenuItemClickListener(this);
         popupMenu.inflate(R.menu.popup_menu);
         popupMenu.show();
